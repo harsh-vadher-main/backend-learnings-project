c71bf91afb7cb45f42d021ab3479752284e9db31
harsh-vadher-main <harsh4vadher@gmail.com>
2025-05-07 11:48:29 +0530

    first api created

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+const app = express();
+
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
@@ -13,5 +15,10 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser())
+
+//routes import  
+import userRouter from './routes/user.routes.js'
+
+app.use('/api/v1/user',userRouter)
 
 export { app };
